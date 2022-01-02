c7c56bf8835cc5a44c17a2efd2935e6eb27ba7e8
nondefault <spam@nondefault.net>
2022-01-02 01:56:33 -0800

    add options

--- index.mjs
+++ index.mjs
@@ -1,10 +1,19 @@
 import { Brush } from "./Brush.mjs";
-import dat from './dat.gui.module.js';
+import dat from "./dat.gui.module.js";
+import { defaultOptions as options } from "./options.mjs";
 
 const resolutionScale = 1.0;
 
 const main = () => {
+  const brush = new Brush();
+
   const gui = new dat.GUI();
+  gui.remember(options);
+  gui.add(options, "lowLatency");
+  gui.add(options, "size").min(1).max(256).onChange(() => {brush.size = options.size});
+  gui.add(options, "hardness").min(0).max(1).onChange(() => {brush.hardness = options.hardness});
+  gui.add(options, "density").min(1).max(16).step(1).onChange(() => {brush.density = options.density});
+
   const display = document.getElementById("canvas");
   const buffer = document.createElement("canvas");
   const displayContext = display.getContext("2d");
@@ -30,8 +39,6 @@ const main = () => {
   let dragging = false;
   let latestPos = [0, 0];
 
-  const brush = new Brush();
-
   const updateDisplay = () => {
     displayContext.clearRect(0, 0, display.width, display.height);
     displayContext.drawImage(buffer, 0, 0);
@@ -42,7 +49,9 @@ const main = () => {
   const onPointerDown = (event) => {
     dragging = true;
     brush.moveTo(eventPos(event));
-    event.preventDefault();
+    if (event.target === display) {
+      event.preventDefault();
+    }
   };
 
   const onPointerUp = (event) => {
@@ -58,10 +67,16 @@ const main = () => {
       brush.strokeTo(bufferContext, eventPos(event));
       event.preventDefault();
     }
+
+    if (options.lowLatency) {
+      updateDisplay();
+    }
   };
 
   requestAnimationFrame(function af() {
-    updateDisplay();
+    if (!options.lowLatency) {
+      updateDisplay();
+    }
     requestAnimationFrame(af);
   });
 
---
+++ options.mjs
@@ -0,0 +1,6 @@
+export const defaultOptions = {
+  lowLatency: false,
+  size: 32,
+  hardness: 0.8,
+  density: 4,
+};
