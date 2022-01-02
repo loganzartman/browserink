c814d91e9eea7b3e9651325c42e7ae6dcde487e7
nondefault <spam@nondefault.net>
2022-01-02 14:46:14 -0800

    clear just clears

--- index.mjs
+++ index.mjs
@@ -13,8 +13,7 @@ const main = () => {
   const snapshotter = new Snapshotter(buffer);
 
   const clear = () => {
-    bufferContext.fillStyle = options.color;
-    bufferContext.fillRect(0, 0, buffer.width, buffer.height);
+    bufferContext.clearRect(0, 0, buffer.width, buffer.height);
   };
   const resize = () => {
     buffer.width = display.width =
@@ -37,7 +36,7 @@ const main = () => {
   const guiEdit = gui.addFolder('Edit');
   guiEdit.add({undo: () => snapshotter.undo()}, "undo");
   guiEdit.add({redo: () => snapshotter.redo()}, "redo");
-  guiEdit.add({clear}, "clear").name('clear to color');
+  guiEdit.add({clear}, "clear");
 
   const eventPos = (event) => [
     (event.pageX - canvas.offsetLeft) *
