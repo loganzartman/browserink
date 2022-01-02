decb5e4df84e298efa9975bb2ee333ffdc8f1faf
nondefault <spam@nondefault.net>
2022-01-02 02:28:56 -0800

    efficient soft brush rendering

--- Brush.mjs
+++ Brush.mjs
@@ -2,16 +2,59 @@ import { lerp } from "./lerp.mjs";
 
 export class Brush {
   constructor() {
+    this._stampTexture = document.createElement('canvas');
+    this._colorizedTexture = document.createElement('canvas');
     this.prevTravelPos = null;
     this.prevPos = null;
     this.travel = 0;
-    this.size = 32;
-    this.hardness = 0.8;
+    this._size = 32;
+    this._hardness = 0.8;
     this.density = 4;
+
+    this._updateStampTexture();
   }
 
-  get splatSpacing() {
+  get size() {
+    return this._size;
+  }
+  set size(size) {
+    this._size = size;
+    this._updateStampTexture();
+  }
+
+  get hardness() {
+    return this._hardness;
+  }
+  set hardness(hardness) {
+    this._hardness = hardness;
+    this._updateStampTexture();
+  }
+  
+  get stampSpacing() {
     return this.size / this.density;
+  }
+
+  _updateStampTexture() {
+    this._stampTexture.width = this._stampTexture.height = this.size;
+    this._colorizedTexture.width = this._colorizedTexture.height = this.size;
+    const c = this._stampTexture.getContext('2d');
+    c.save();
+    c.scale(this.size, this.size);
+    const fill = c.createRadialGradient(0.5, 0.5, 0, 0.5, 0.5, 0.5);
+    fill.addColorStop(this.hardness, 'rgba(255,255,255,1)');
+    fill.addColorStop(1, 'rgba(255,255,255,0)');
+    c.fillStyle = fill;
+    c.fillRect(0, 0, 1, 1);
+    c.restore();
+  }
+
+  _updateColorizedTexture(tint) {
+    const c = this._colorizedTexture.getContext('2d');
+    c.globalCompositeOperation = 'copy';
+    c.fillStyle = tint;
+    c.fillRect(0, 0, this.size, this.size);
+    c.globalCompositeOperation = 'destination-in';
+    c.drawImage(this._stampTexture, 0, 0, this.size, this.size);
   }
 
   _updateTravel(pos) {
@@ -22,15 +65,14 @@ export class Brush {
     this.travel += dist;
   }
 
-  _splat(context, params) {
+  _stamp(context, params) {
     const c = context;
     c.save();
     c.translate(params.x, params.y);
     c.scale(params.size, params.size);
 
-    c.beginPath();
-    c.ellipse(0, 0, 1, 1, 0, 0, Math.PI * 2);
-    c.fill();
+    this._updateColorizedTexture(params.color);
+    c.drawImage(this._colorizedTexture, -0.5, -0.5, 1, 1);
 
     c.restore();
   }
@@ -39,16 +81,18 @@ export class Brush {
     const c = context;
     c.save();
 
+    const s = this.size * 0.5;
+
     c.strokeStyle = 'white';
     c.lineWidth = 3;
     c.beginPath();
-    c.ellipse(...pos, this.size, this.size, 0, 0, Math.PI * 2);
+    c.ellipse(...pos, s, s, 0, 0, Math.PI * 2);
     c.stroke();
     
     c.strokeStyle = 'black';
     c.lineWidth = 1;
     c.beginPath();
-    c.ellipse(...pos, this.size, this.size, 0, 0, Math.PI * 2);
+    c.ellipse(...pos, s, s, 0, 0, Math.PI * 2);
     c.stroke();
     
     c.restore();
@@ -61,7 +105,7 @@ export class Brush {
 
   strokeTo(context, pos) {
     this._updateTravel(pos);
-    if (this.travel < this.splatSpacing) {
+    if (this.travel < this.stampSpacing) {
       return;
     }
 
@@ -75,17 +119,14 @@ export class Brush {
       y: pos[1],
       size: this.size,
     };
+    const color = 'red';
 
-    const splatSteps = Math.floor(this.travel / this.splatSpacing);
-    for (let i = 0; i < splatSteps; ++i) {
-      const blend = lerp(a, b, (i + 1) / splatSteps);
-      const fill = context.createRadialGradient(0, 0, 0, 0, 0, 1);
-      fill.addColorStop(this.hardness, `hsla(${Date.now()*0.1%360},50%,50%,1.0)`);
-      fill.addColorStop(1, `hsla(${Date.now()*0.1%360},50%,50%,0.0)`);
-      context.fillStyle = fill;
-      this._splat(context, blend);
+    const stampSteps = Math.floor(this.travel / this.stampSpacing);
+    for (let i = 0; i < stampSteps; ++i) {
+      const blend = lerp(a, b, (i + 1) / stampSteps);
+      this._stamp(context, {...blend, color});
     }
-    this.travel %= this.splatSpacing;
+    this.travel %= this.stampSpacing;
     this.prevPos = pos;
   }
 }
