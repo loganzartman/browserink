2cbe6ee2f41522db11e4d36863227d8dbd3b250a
nondefault <spam@nondefault.net>
2022-01-02 21:30:07 -0800

    change gradient shape

--- drawGradient.mjs
+++ drawGradient.mjs
@@ -1,4 +1,6 @@
 const easeInOutSine = (f) => -(Math.cos(Math.PI * f) - 1) / 2;
+const easeInOutCubic = (f) => f < 0.5 ? 4 * f * f * f : 1 - Math.pow(-2 * f + 2, 3) / 2;
+const easeInOutQuad = (f) => f < 0.5 ? 2 * f * f : 1 - Math.pow(-2 * f + 2, 2) / 2;
 const clamp = (f, a, b) => Math.max(a, Math.min(b, f));
 
 export const drawGradient = ({context, size, hardness, dither=0}={}) => {
@@ -12,7 +14,7 @@ export const drawGradient = ({context, size, hardness, dither=0}={}) => {
       const fx = (x - half) / half;
       const fy = (y - half) / half;
       const f = (1 / softness) - (Math.sqrt(fx ** 2 + fy ** 2) / softness);
-      const alpha = easeInOutSine(clamp(f + Math.random() * dither * 0.5 - dither, 0, 1));
+      const alpha = easeInOutQuad(clamp(f + Math.random() * dither * 0.5 - dither, 0, 1));
 
       const index = (y * size + x) * 4;
       data.data[index+0] = 255;
