ab5246e81df166cb14fe66435fbf5494f55a0160
nondefault <spam@nondefault.net>
2022-04-04 21:32:59 -0700

    improve debug rendering

--- DebugRenderer.mjs
+++ DebugRenderer.mjs
@@ -1,3 +1,18 @@
+// creates a JSON.stringify replacer function that stops recursion
+// when it encounters circular references.
+const noCyclesReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return '[[Circular]]';
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
 export class DebugRenderer {
   constructor(eventNode) {
     this.eventNode = eventNode;
@@ -9,22 +24,27 @@ export class DebugRenderer {
   }
   
   render(context) {
+    const fontSize = 20;
     context.save();
     context.translate(16, 16);
-    context.font = '24px monospace';
+    context.font = `${fontSize}px monospace`;
     context.textAlign = 'left';
     context.textBaseline = 'top';
+    context.lineJoin = 'round';
 
     let i = 0;
     let y = 0;
     for (const [key, value] of this.textValues) {
-      const text = `${key}: ${JSON.stringify(value, undefined, 2)}`;
+      const text = `${key}: ${JSON.stringify(value, noCyclesReplacer(), 2)}`;
       const metrics = context.measureText(text);
-      const height = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
-      context.fillStyle = `hsl(${i * 360 / this.textValues.size}, 100%, 50%)`;
-      context.lineWidth = 2;
-      context.strokeStyle = 'white';
+      let height = fontSize;
+      if (metrics.fontBoundingBoxAscent) {
+        height = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
+      }
+      context.fillStyle = `hsl(${i * 360 / this.textValues.size}, 100%, 80%)`;
       for (const line of text.split('\n')) {
+        context.lineWidth = 8;
+        context.strokeStyle = 'rgba(0,0,0,0.8)';
         context.strokeText(line, 0, y);
         context.fillText(line, 0, y);
         y += height;
